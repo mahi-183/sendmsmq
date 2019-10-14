0faa47bfdbc9a5f7ba940ca8a95044508aceba37
mahi-183 <Poojita38@gmail.com>
2019-10-14 16:43:07 +0530

    14Sep2019

---
+++ SendRecieveMSMQ/EmilService.cs
@@ -0,0 +1,87 @@
+﻿
+namespace SendRecieveMSMQ
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Mail;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class EmilService
+    {
+        public void EmailService(string ToMail,string token)
+        { 
+            // This address must be verified with User Login registration.
+            string FROM = "maheshaurad183@gmail.com";
+            string FROMNAME = "FundooApp";
+
+            // smtp_username with SMTP user name.
+            string SMTP_USERNAME = "maheshaurad183@gmail.com";
+
+            // Replace smtp_password with your Amazon SES SMTP user name.
+            string SMTP_PASSWORD = "7720009867";
+
+            // (Optional) the name of a configuration set to use for this message.
+            // If you comment out this line, you also need to remove or comment out
+            // the "X-SES-CONFIGURATION-SET" header below.
+            //string CONFIGSET = "ConfigSet";
+
+            // If you're using Amazon SES in a region other than US West (Oregon), 
+            // replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP  
+            // endpoint in the appropriate AWS Region.
+            string HOST = "smtp.gmail.com";
+
+            // The port you will connect to on the Amazon SES SMTP endpoint. We
+            // are choosing port 587 because we will use STARTTLS to encrypt
+            // the connection.
+            int PORT = 587;
+
+            // The subject line of the email
+            string SUBJECT =
+                "Forgot Password";
+
+            // The body of the email
+            string BODY =
+                "<h1>Please Click on the below link to reset your password</h1>" +
+                "<p>" +
+                "<a href='http://localhost:4200/resetPassword/'+ token>Click here</a>" + 
+                "</p>";
+
+            // Create and build a new MailMessage object  
+            MailMessage message = new MailMessage();
+            message.IsBodyHtml = true;
+            message.From = new MailAddress(FROM, FROMNAME);
+            message.To.Add(new MailAddress(ToMail));
+            message.Subject = SUBJECT;
+            message.Body = BODY;
+            // Comment or delete the next line if you are not using a configuration set
+           // message.Headers.Add("X-SES-CONFIGURATION-SET", CONFIGSET);
+
+            using (var client = new System.Net.Mail.SmtpClient(HOST, PORT))
+            {
+                // Pass SMTP credentials
+                client.Credentials =
+                    new NetworkCredential(SMTP_USERNAME, SMTP_PASSWORD);
+
+                // Enable SSL encryption
+                client.EnableSsl = true;
+
+                // Try to send the message. Show status in console.
+                try
+                {
+                    Console.WriteLine("Attempting to send email...");
+                    client.Send(message);
+                    Console.WriteLine("Email sent!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The email was not sent.");
+                    Console.WriteLine("Error message: " + ex.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ SendRecieveMSMQ/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Messaging;
+using Experimental.System.Messaging;
+using SendRecieveMSMQ;
+
+namespace SendReceiveMSMQ
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //path of queue
+            const string queueName = @".\private$\Mail";
+
+            //create event Messagelister object 
+            MSMQListner msmq = new MSMQListner(queueName);
+
+            //message formatterType is string
+            msmq.FormatterTypes = new Type[] { typeof(string) };
+            
+            //call to clickEvent MessaggeReceivedEventHandler method  
+            msmq.MessageReceived += new MessageReceivedEventHandler(listener_MessageReceived);
+            msmq.Start();
+          
+            //FireCommand(queueName);
+            Console.ReadLine();
+        }
+
+        static void listener_MessageReceived(object sender, MessageEventArgs args)
+        {
+            Console.WriteLine("received");
+        }
+    }
+
+    //delegate method MessageReceivedEventHandler
+    public delegate void MessageReceivedEventHandler(object sender, MessageEventArgs args);
+
+    public class MSMQListner
+    {
+        private bool _listen;
+        private Type[] _types;
+        private MessageQueue _queue;
+
+        public event MessageReceivedEventHandler MessageReceived;
+
+        public Type[] FormatterTypes
+        {
+            get { return _types; }
+            set { _types = value; }
+        }
+
+        public MSMQListner(string queuePath)
+        {
+            _queue = new MessageQueue(queuePath);
+        }
+
+        public void Start()
+        {
+            _listen = true;
+
+            if (_types!=null && _types.Length>0)
+            {
+                //Using only the XmlMessageFormatter. You can use other formatter as well
+                _queue.Formatter = new XmlMessageFormatter(_types);
+            }
+
+            //_queue.PeekCompleted += new PeekCompletedEventHandler(OnPeekCompleted);
+            _queue.ReceiveCompleted += new ReceiveCompletedEventHandler(OnReceiveCompleted);
+
+            StartListening();
+        }
+
+        public void Stop()
+        {
+            _listen = false;
+            _queue.ReceiveCompleted -= new ReceiveCompletedEventHandler(OnReceiveCompleted);
+
+        }
+    
+        private void StartListening()
+        {
+            if (!_listen)
+            {
+                return;
+            }
+
+            // The MSMQ class does not have a BeginRecieve method that can take in a 
+            // MSMQ transaction object. This is a   – we do a BeginPeek and then 
+            // recieve the message synchronously in a transaction.
+            // Check documentation for more details
+            if (_queue.Transactional)
+            {
+                _queue.BeginPeek();
+            }
+            else
+            {
+                _queue.BeginReceive();
+            }
+        }
+
+        private void FireRecieveEvent(object body)
+        {
+            if (MessageReceived != null)
+            {
+                MessageReceived(this, new MessageEventArgs(body));
+            }
+        }
+       
+            private void OnReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
+            {
+                Message msg = _queue.EndReceive(e.AsyncResult);
+            
+                Console.WriteLine("MEssage:::" + msg.Body);
+                EmilService emailService = new EmilService();
+                emailService.EmailService(msg.Body.ToString(), msg.Label);
+
+                StartListening();
+
+                FireRecieveEvent(msg.Body);
+            }
+        }
+    }
+
+    public class MessageEventArgs : EventArgs
+    {
+        private object _messageBody;
+
+        public object MessageBody
+        {
+            get { return _messageBody; }
+        }
+
+        public MessageEventArgs(object body)
+        {
+            _messageBody = body;
+
+        }
+    }
